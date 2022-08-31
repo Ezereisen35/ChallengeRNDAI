2a8f4584530011df8825c81f61d23d839cd8c18b
Ezereisen35 <101258636+Ezereisen35@users.noreply.github.com>
2022-08-31 10:28:00 -0300

    a

---
+++ screens/Home.jsx
@@ -0,0 +1,22 @@
+import token from './form';
+import { StyleSheet, Text, Image , View, TextInput, FlatList, StatusBar, SafeAreaView, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import NombreDelPlato from '../components/NombreDelPlato';
+import ImagenDelPlato from '../components/ImagenDelPlato';
+
+const Home = ({ navigation }) => {
+
+return(
+
+<View>
+<ImagenDelPlato/>
+
+</View>
+
+)
+    
+    
+}
+
+export default Home;
